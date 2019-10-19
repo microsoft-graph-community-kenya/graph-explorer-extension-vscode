86cfe57038392f404a9e6b601ea472f4bc256610
Japheth Obala <japhethobalak@gmail.com>
2019-10-19 10:51:39 +0300

    Deletes most of the code in these files

--- src/view/SamplesProvider.ts
+++ src/view/SamplesProvider.ts
@@ -1,39 +1,18 @@
 import * as vscode from 'vscode';
 
 import { TreeQuery } from './TreeQuery';
+import { ISample } from '../types';
 
-const samples = [
-  'my profile',
-  'may photo',
-  'my mail',
-  'all the items in my drive'
-];
-export default class SampleQueryProvider
-  implements vscode.TreeDataProvider<TreeQuery> {
-  constructor(private context: vscode.ExtensionContext) {}
+export default class SamplesProvider {
+  private samples: ISample[];
 
-  getChildren(query?: TreeQuery): TreeQuery[] {
-    const sampleQueries: TreeQuery[] = [];
-    for (const item of samples) {
-      sampleQueries.push(
-        new TreeQuery(
-          'GET',
-          item,
-          vscode.TreeItemCollapsibleState.None,
-          '/Users/rey/Documents/Github/openS/graph-explorer/media/get.png',
-          { 
-            command: 'sampleQuery.Click', 
-            title: "click", 
-            arguments: [item]
-       }
-          
-        )
-      );
-    }
-    return sampleQueries;
+  constructor(samples: ISample[]) {
+    this.samples = samples;
   }
 
-  getTreeItem(query: TreeQuery): vscode.TreeItem {
-    return query;
+  private groupSamplesByCategory(samples: ISample[]): object[] {
+    return samples.reduce((arr: any, sample) => {
+
+    }, []);
   }
 }
--- src/view/sideBar.ts
+++ src/view/sideBar.ts
@@ -5,67 +5,12 @@ import SampleQueryProvider from './SamplesProvider';
 export default class SideBar {
   private sampleQueries: any;
   constructor(context: ExtensionContext) {
-    const sampleQueriesProvider = new SampleQueryProvider(context);
-
-    this.sampleQueries = window.registerTreeDataProvider(
-      'sample-queries',
-      sampleQueriesProvider
-	);
-
-    // register a content provider for the cowsay-scheme
-    const myScheme = 'response';
-    const myProvider = new (class
-      implements vscode.TextDocumentContentProvider {
-      // emitter and its event
-      onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
-      onDidChange = this.onDidChangeEmitter.event;
-
-      provideTextDocumentContent(uri: vscode.Uri): string {
-        return uri.query;
+    const samples = [
+      {
+        'Getting Started': ['my mail', 'items trending around me', 'all items in my drive']
       }
-    })();
-    vscode.workspace.registerTextDocumentContentProvider(myScheme, myProvider);
+    ];
 
-    commands.registerCommand('sampleQuery.Click', async query => {
-      let uri = vscode.Uri.parse(
-        'response:' +
-          `response.json?` +
-          JSON.stringify({
-            'my profile': {
-              '@odata.context':
-                'https://graph.microsoft.com/v1.0/$metadatausers/$entity\n',
-              businessPhones: ['+1 412 555 0109'],
-              displayName: 'Megan Bowen\n',
-              givenName: 'Megan',
-              jobTitle: 'Auditor',
-              mail: 'MeganB@M365x214355.onmicrosoft.com',
-              mobilePhone: null,
-              officeLocation: '12/1110',
-              preferredLanguage: 'en-US',
-              surname: 'Bowen',
-              userPrincipalName: 'MeganB@M365x214355.onmicrosoft.com',
-              id: '48d31887-5fad-4d73-a9f5-3c356e68a038'
-            }
-            // add new lines and tabs
-          }).replace(/,/g, ',\n\t')
-      );
-      let doc = await vscode.workspace.openTextDocument(uri); // calls back into the provider
-
-      await vscode.window.showTextDocument(doc, {});
-      if (!vscode.window.activeTextEditor) {
-        return;
-      }
-
-      let { document, edit } = vscode.window.activeTextEditor;
-
-      // formats the response
-      edit(builder => {
-        const start = new vscode.Position(0, 0);
-
-        const end = new vscode.Position(document.lineCount, document.eol);
-        const allRange = new vscode.Range(start, end);
-        builder.replace(allRange, document.getText());
-      });
-    });
+    const samplesProvider = new SampleQueryProvider(samples as any);
   }
 }
