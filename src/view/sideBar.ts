5ef918a8fef06281785044167c9591475de322d7
Japheth Obala <japhethobalak@gmail.com>
2019-10-20 13:35:00 +0300

    Fetch code snippets

--- src/view/sideBar.ts
+++ src/view/sideBar.ts
@@ -4,6 +4,8 @@ import SampleQueryProvider from './SamplesProvider';
 import { samples } from './samples/samples';
 import { ISample } from '../types';
 
+const request = require('request-promise');
+
 export default class Sidebar {
   constructor() {
     this.showSamplesTreeView();
@@ -23,7 +25,7 @@ export default class Sidebar {
 
   private getSnippet(language: string, sample: ISample) {
     const httpMsg = this.generateHttpMsg(language, sample);
-    console.log(httpMsg);
+    this.loadSnippet(httpMsg);
   }
 
   private generateHttpMsg(language: string, sample: ISample): string {
@@ -33,14 +35,26 @@ export default class Sidebar {
       sample.requestUrl = urlObject.pathname + urlObject.search;
     }
 
-    let url = 'https://graphexplorerapi.azurewebsites.net/api/graphexplorersnippets';
-
-    if (language !== 'csharp') {
-      url += `?lang=${language}`;
-    }
-
     // tslint:disable-next-line: max-line-length
     const body = `${sample.method} ${sample.requestUrl} HTTP/1.1\r\nHost: graph.microsoft.com\r\nContent-Type: application/json\r\n\r\n${JSON.stringify(sample.postBody)}`;
     return body;
+  }
+
+  private loadSnippet(httpMsg: string, language='csharp') {
+    let uri = 'https://graphexplorerapi.azurewebsites.net/api/graphexplorersnippets';
+
+    if (language !== 'csharp') {
+      uri += `?lang=${language}`;
+    }
+
+    request({
+      method: 'POST',
+      uri,
+      body: httpMsg
+    }, function (error: any, response: any, body: any) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log(body); // Print the HTML for the Google homepage.
+    });
   }
 }
